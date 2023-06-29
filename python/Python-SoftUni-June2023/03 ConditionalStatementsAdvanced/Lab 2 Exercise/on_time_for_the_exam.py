49bf495f3b60cef80662accf85ebb3be65bee3d0
ozazza <ozvacheva@gmail.com>
2023-06-29 18:56:12 +0300

    Added last tasks - wrong result task 8

--- python/Python-SoftUni-June2023/03 ConditionalStatementsAdvanced/Lab 2 Exercise/on_time_for_the_exam.py
+++ python/Python-SoftUni-June2023/03 ConditionalStatementsAdvanced/Lab 2 Exercise/on_time_for_the_exam.py
@@ -4,8 +4,46 @@ exam_m = int(input())
 arrive_h = int(input())
 arrive_m = int(input())
 
-
+status = ''
+diff_minutes = 0
+hours = 0
+minutes = 0
 
 # Logic
+# Convert times to minutes
+arrive_to_min = (arrive_h * 60) + arrive_m
+exam_maximum_to_min = (exam_h * 60) + exam_m
+exam_minimum_to_min = exam_maximum_to_min - 30
+
+# Calculate Difference
+diff_minutes = arrive_to_min - exam_maximum_to_min
+
+# Determine the Statuses
+if diff_minutes > 0:
+    status = 'Late'
+elif exam_minimum_to_min <= arrive_to_min <= exam_maximum_to_min:
+    status = 'On time'
+elif exam_minimum_to_min > arrive_to_min:
+    status = 'Early'
+
+# Calculate hours and minutes
+hours = abs(int(diff_minutes / 60))
+# hours2 = abs(diff_minutes / 60)
+
+minutes = abs(diff_minutes / 60) + abs(diff_minutes % 60)
 
 # Output
+print(status)
+
+if diff_minutes != 0:
+
+    if abs(diff_minutes) < 60:
+        print(f'{abs(diff_minutes)} minutes', end=' ')
+
+    elif abs(diff_minutes) >= 60:
+        print(f'{hours}:{minutes:02d} hours', end=' ')
+
+    if diff_minutes < 0:
+        print(f'before the start')
+    elif diff_minutes > 0:
+        print(f'after the start')
