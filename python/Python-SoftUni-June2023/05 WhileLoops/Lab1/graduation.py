3c447b590724a3b2031b7721166c9d0db2c472a2
ozazza <ozvacheva@gmail.com>
2023-07-03 23:15:30 +0300

    M 05 Ready

--- python/Python-SoftUni-June2023/05 WhileLoops/Lab1/graduation.py
+++ python/Python-SoftUni-June2023/05 WhileLoops/Lab1/graduation.py
@@ -2,8 +2,7 @@
 student = input()
 
 # Logic
-next_class_counter = 1
-class_pass = 0
+next_class = 1
 total_grade = 0
 average_grade = 0
 failed = 0
@@ -16,16 +15,51 @@ while True:
 
         if failed == 2:
             # suspended = True
-            print(f'{student} has been excluded at {class_pass} grade')
+            print(f'{student} has been excluded at {next_class} grade')
             break
 
         continue
 
-    class_pass += 1
-    next_class_counter = class_pass + 1
+    next_class += 1
     total_grade += grade
-    average_grade = total_grade / class_pass
 
-    if class_pass == 12:
+    if next_class > 12:
+
+        average_grade = total_grade / (next_class - 1)
+
         print(f'{student} graduated. Average grade: {average_grade:.2f}')
         break
+
+# # Input
+# student = input()
+#
+# # Logic
+# next_class_counter = 1
+# class_pass = 0
+# total_grade = 0
+# average_grade = 0
+# failed = 0
+#
+# while True:
+#     grade = float(input())
+#
+#     if grade < 4:
+#         failed += 1
+#
+#         if failed == 2:
+#             # suspended = True
+#             print(f'{student} has been excluded at {next_class_counter} grade')
+#             break
+#
+#         continue
+#
+#     class_pass += 1
+#     next_class_counter = class_pass + 1
+#     total_grade += grade
+#
+#     if class_pass == 12:
+#
+#         average_grade = total_grade / 12
+#
+#         print(f'{student} graduated. Average grade: {average_grade:.2f}')
+#         break
